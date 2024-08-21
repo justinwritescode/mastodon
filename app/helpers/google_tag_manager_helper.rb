6503bb1d0ee8c223d792920f06c416818b7a21dd
David Gerard <justin@justinwritescode.com>
2024-08-21 09:28:00 -0400

    Update google_tag_manager_helper.rb

--- app/helpers/google_tag_manager_helper.rb
+++ app/helpers/google_tag_manager_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module GoogleTagManagerHelper
   def google_tag_manager(gtm_id)
     return '' if gtm_id.blank?
 
     javascript_tag nonce: true do
-      <<-JS.strip_heredoc.html_safe
+      <<~JS.strip_heredoc#.html_safe
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
