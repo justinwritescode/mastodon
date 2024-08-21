235b5976993075a2aa4905dc0a747dcfbbc206ae
David Gerard <justin@justinwritescode.com>
2024-08-21 09:07:14 -0400

    Yarn updates and Google Tag Manager plug-in.

---
+++ app/helpers/google_tag_manager_helper.rb
@@ -0,0 +1,15 @@
+module GoogleTagManagerHelper
+  def google_tag_manager(gtm_id)
+    return '' if gtm_id.blank?
+
+    javascript_tag nonce: true do
+      <<-JS.strip_heredoc.html_safe
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','#{gtm_id}');
+      JS
+    end
+  end
+end
