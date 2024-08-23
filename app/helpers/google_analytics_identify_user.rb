aa41a8f2c5dc11b4db75e43c0145b4c73b1da26d
David Gerard <justin@justinwritescode.com>
2024-08-22 22:26:09 -0400

    fixing Google Analytics identify user plug-in

--- app/helpers/google_analytics_identify_user.rb
+++ app/helpers/google_analytics_identify_user.rb
@@ -1,10 +1,12 @@
-module GoogleAnalyticsHelper
+# frozen_string_literal: true
+
+module GoogleAnalyticsIdentifyUser
   def google_analytics_identify_user
     if user_signed_in?
       user_id = current_user.username # or other unique identifier
 
       javascript_tag do
-        <<-JS.html_safe
+        <<-JS
           gtag('config', '#{Rails.application.credentials.google_analytics[:tracking_id]}', { 'user_id': '#{user_id}' });
         JS
       end
