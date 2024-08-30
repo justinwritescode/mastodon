0f4b0d3c167f9fc4dd852d7ecef981a17c3836de
Justin P. Chase <justin@justinwritescode.com>
2024-08-30 08:03:38 -0400

    Update entrypoint.sh

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
 
-cd /mastodon && \
-    bin/bundle exec rails server -p 3000
-    bash /start-sshd.sh
-    bash /start-nginx.sh
+set -e
+export RAILS_ENV=production
+export PATH="/mastodon/bin:${PATH}"
+
+if [[ $PROGRAM == "mastodon" ]]; then
+    bash /start-mastodon.sh;
+elif [[ $PROGRAM == "sidekiq" ]]; then
+    bash /start-sidekiq.sh;
+elif [[ $PROGRAM == "streaming" ]]; then
+    bash /start-streaming.sh;
+else
+    echo "Program '${PROGRAM}' could not be started.";
+fi
