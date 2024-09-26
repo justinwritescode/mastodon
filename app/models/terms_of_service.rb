b61a70324757bcca48d84dbd3caef495b3a8d7da
HIV CHASER <chaser@poz.world>
2024-09-26 04:01:48 -0400

    Change TOS to privacy policy and make a new real TOS

--- app/models/terms_of_service.rb
+++ app/models/terms_of_service.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-class PrivacyPolicy < ActiveModelSerializers::Model
+class TermsOfService < ActiveModelSerializers::Model
   DEFAULT_PRIVACY_POLICY = Rails.root.join('config', 'templates', 'privacy-policy.md').read
   DEFAULT_UPDATED_AT = DateTime.new(2022, 10, 7).freeze
 
   attributes :updated_at, :text
 
   def self.current
-    custom = Setting.find_by(var: 'site_privacy_policy')
+    custom = Setting.find_by(var: 'site_terms')
 
     if custom&.value.present?
       new(text: custom.value, updated_at: custom.updated_at)
